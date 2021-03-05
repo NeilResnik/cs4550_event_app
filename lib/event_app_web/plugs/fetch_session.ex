36fbfa4b8ac0bb13670fc21cca276a6dab9954ba
Neil Resnik <neilr22@comcast.net>
2021-03-04 21:28:49 -0500

    Fix some module name issues

--- lib/event_app_web/plugs/fetch_session.ex
+++ lib/event_app_web/plugs/fetch_session.ex
@@ -9,7 +9,7 @@ defmodule EventAppWeb.Plugs.FetchUser do
 
   def call(conn, _args) do
     user_id = get_session(conn, :user_id) || -1
-    user = PhotoBlog.Users.get_user(user_id)
+    user = EventApp.Accounts.get_user(user_id)
     if user do
       assign(conn, :current_user, user)
     else
